3ee33eaaa065969d14f364c1eb523172fffb1016
Srikanth_Attacker <fireboy024@gmail.com>
2022-12-07 18:08:08 +0530

    message

--- Bank.java
+++ Bank.java
@@ -1,34 +1,7 @@
-class Bank
-{
-	String name="SBI";
-	private int minBalance=5000;
-	float interest=3.5f;
-	
-	public void setminBalance(int amount)
-	{
-		if(amount>5000)
-		{
-			this.minBalance=amount;
-		}
-	}
-	
-	public int display()
-	{
-		return this.minBalance;
-	}
-	
-	public static void main(String[] args)
-	{
-		Bank branchManager=new Bank();
-		System.out.println(branchManager.name);
-		branchManager.openAccount();
-	}
-	public void openAccount()
-	{
-		System.out.println("Check");
-	}
-	private void closeAccount()
-	{
-		System.out.println("Account closed");
-	}
-}
+lkkjhkjhjkehwf
+jbhjfbwbf
+jbfiar
+mnbsdjba
+sdblb;i
+nskdnco
+mnadskjn
